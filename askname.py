d6f5b1ba26ebb925cedd71c825ae47e0505430b7
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-08-14 09:33:38 +0000

    Created askname.

---
+++ askname.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+name = raw_input('What is your name? ')
+if name == '':
+    print 'So, you want to be anonymous, eh?'
+else:
+    print 'Hi %s, and welcome to Python!' % name

7ff7b6bed453128e234e586e738d2901f7f2a52b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2009-08-25 00:11:15 +0000

    The call to pwd.getpwuid() can fail. Deal with it.

--- whoami.py
+++ whoami.py
@@ -4,6 +4,11 @@ import os, pwd, sys
 
 app = os.path.basename(sys.argv[0])
 uid = os.getuid()
-pw = pwd.getpwuid(uid)
+
+try:
+    pw = pwd.getpwuid(uid)
+except KeyError:
+    print '%s: failed to find user name for %lu' % (app, uid)
+    sys.exit(1)
 
 print pw.pw_name
